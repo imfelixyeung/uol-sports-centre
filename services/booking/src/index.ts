fd737c29d7b533242a1418baed49002d329da126
William Neild <wneild5@gmail.com>
2023-02-27 13:50:12 +0000

    Added jest unit testing

--- services/booking/src/index.ts
+++ services/booking/src/index.ts
@@ -3,15 +3,16 @@ import {env} from './env';
 import logger from './logger';
 import bookingRouter from './router/booking';
 
-const main = () => {
+export const createServer = (): express.Express => {
   const app = express();
 
   app.use('/', bookingRouter);
 
-  app.listen(env.PORT, env.HOST, () => {
-    logger.info(`Listening on http://${env.HOST}:${env.PORT}`);
-  });
+  return app;
 };
 
-// run the main function
-main();
+// run the main server
+const app = createServer();
+app.listen(env.PORT, env.HOST, () => {
+  logger.info(`Listening on http://${env.HOST}:${env.PORT}`);
+});
---
+++ services/booking/tests/booking.test.ts
@@ -0,0 +1,31 @@
+import {beforeAll, describe, expect, test} from '@jest/globals';
+import express from 'express';
+import supertest from 'supertest';
+import {createServer} from '../src/index';
+
+let app: express.Express;
+
+beforeAll(done => {
+  app = createServer();
+  done();
+});
+
+describe('Test', () => {
+  test('GET /bookings', async () => {
+    // manually add booking to test db
+    // ~~~add booking~~~
+
+    // perform test to see if it is there
+    await supertest(app)
+      .get('/bookings')
+      .expect(200)
+      .then(response => {
+        // Check type and length
+        expect(Array.isArray(response.body)).toBeTruthy();
+        expect(response.body.length).toEqual(1);
+
+        // Check data matches with the record we added
+        // expect(response.body[0]._id).toBe(booking.id);
+      });
+  });
+});
