bfc2e747af7678cd4ae31a2c351a8084d8bdc4b9
Savan Dosanjh <sc21srsd@leeds.ac.uk>
2023-02-28 21:21:18 +0000

    feat: Started checkout session page functionality

--- services/payments/payments.py
+++ services/payments/payments.py
@@ -6,17 +6,26 @@ from flask import Flask
 app = Flask(__name__)
 
 stripe.api_key = "APIKEY"
-stripe.product.create(
+#stripe.product.create(
     #To Do: create products for bookings and subscriptions
-)
-stripe.price.create(
+#)
+#stripe.price.create(
     #To Do: create prices for annual and monthly memberships
     #and bookings
-)
+#)
 
 @app.route("/checkout-session", methods=['POST'])
 def createCheckout():
     '''Create checkout session for purchasing bookings/subscriptions using Stripe'''
+    checkoutSession = stripe.checkout.Session.create(
+        successUrl = "paymentSuccessPage",
+        line_items=[
+        {
+            "price": "subscriptionPage",
+            "quantity": 1
+        },],
+        mode = 'subscription',
+    )
 
 @app.route('/webhook', methods=['POST'])
 def webhookReceived():
