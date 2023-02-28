0ebacd890c539fe95b59d5eeef1bd3b4de14fe54
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2023-02-28 15:53:41 +0000

    Created flask model for the facilities Database

--- services/facilities/app/models.py
+++ services/facilities/app/models.py
@@ -1,6 +1,35 @@
 from app import db
 
-
+# Model for the Facilities table
 class Facility(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(128))
+    capacity = db.Column(db.Integer)
+
+    # Foreign Connectors for the Facilities table
+    openTimes = db.relationship('openTime', backref='facility', lazy='dynamic')
+    activities = db.relationship('activity', backref='facility', lazy='dynamic')
+
+# Model for the OpenTime table
+# The openingTime and closingTime fields are stored as minutes after midnight.
+# For example: if "football" opened at 11:30am the opening time would be stored as 690
+class OpenTime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String(10))
+    openingTime = db.Column(db.Integer)
+    closingTime = db.Column(db.Integer)
+
+    # Foreign Connectors for the OpenTimes table
+    facility_id = db.Column(db.Integer, db.ForeignKey('facility.id'))
+
+# Model for the Activity table
+# The duration field is stored as mileseconds to help with communication to other microservices
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    duration = db.Column(db.Integer)
+    capacity = db.Column(db.Integer)
+
+    # Foreign Connectors for the Activity table
+    facility_id = db.Column(db.Integer, db.ForeignKey('facility.id'))
+    
+
