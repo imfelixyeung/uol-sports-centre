fc01630142417eccd0bfe807b3102297d6d3047a
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-01 06:42:28 +0000

    feat: add auth endpoints

--- services/web/src/redux/services/api.ts
+++ services/web/src/redux/services/api.ts
@@ -1,4 +1,13 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {
+  GetSessionResponse,
+  LoginRequest,
+  LoginResponse,
+  RefreshTokenRequest,
+  RefreshTokenResponse,
+  RegisterRequest,
+  RegisterResponse,
+} from './types/auth';
 import {StatusReportResponse} from './types/status';
 
 export const api = createApi({
@@ -8,10 +17,51 @@ export const api = createApi({
   }),
   tagTypes: [],
   endpoints: builder => ({
+    login: builder.mutation<LoginResponse, LoginRequest>({
+      query: credentials => ({
+        url: '/auth/login',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+
+    register: builder.mutation<RegisterResponse, RegisterRequest>({
+      query: credentials => ({
+        url: '/auth/register',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+
+    logout: builder.mutation<void, void>({
+      query: () => ({
+        url: '/auth/logout',
+        method: 'POST',
+      }),
+    }),
+
+    getSession: builder.query<GetSessionResponse, void>({
+      query: () => '/auth/session',
+    }),
+
+    refreshToken: builder.mutation<RefreshTokenResponse, RefreshTokenRequest>({
+      query: () => ({
+        url: '/auth/token',
+        method: 'POST',
+      }),
+    }),
+
     getStatusReport: builder.query<StatusReportResponse, void>({
       query: () => '/status/report',
     }),
   }),
 });
 
-export const {useGetStatusReportQuery} = api;
+export const {
+  useGetStatusReportQuery,
+  useGetSessionQuery,
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useRefreshTokenMutation,
+} = api;
