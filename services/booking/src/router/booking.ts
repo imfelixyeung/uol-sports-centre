da246edc0231990d8fb94d5ed68cde560b3744f9
William Neild <wneild5@gmail.com>
2023-03-01 09:39:16 +0000

    add 404 and 5xx error handling + moved router for bookings from `/` to `/bookings`

--- services/booking/src/router/booking.ts
+++ services/booking/src/router/booking.ts
@@ -5,18 +5,18 @@ import BookingController from '../controllers/booking.controller';
 const bookingRouter: Router = express.Router();
 
 // get all bookings
-bookingRouter.get('/bookings', BookingController.getBookings);
+bookingRouter.get('/', BookingController.getBookings);
 
 // create new booking
-bookingRouter.post('/bookings', BookingController.createBooking);
+bookingRouter.post('/', BookingController.createBooking);
 
 // get specific booking
-bookingRouter.get('/booking/:id', BookingController.getBookingById);
+bookingRouter.get('/:id', BookingController.getBookingById);
 
 // update specific booking
-bookingRouter.post('/booking/:id', BookingController.updateBookingById);
+bookingRouter.post('/:id', BookingController.updateBookingById);
 
 // delete specific booking
-bookingRouter.delete('/booking/:id', BookingController.deleteBookingById);
+bookingRouter.delete('/:id', BookingController.deleteBookingById);
 
 export default bookingRouter;
--- services/booking/src/server.ts
+++ services/booking/src/server.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import bookingRouter from './router/booking';
+import {notFoundHandler, serverErrorHandler} from './router/error';
 
 export const createServer = (): express.Express => {
   const app = express();
 
-  app.use('/', bookingRouter);
+  // routers
+  app.use('/bookings', bookingRouter);
+
+  //
+  // error handling
+  //
+
+  // 404 - Not found
+  app.use(notFoundHandler);
+
+  // 5xx - Actual error
+  app.use(serverErrorHandler);
 
   return app;
 };
