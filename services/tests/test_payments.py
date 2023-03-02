1bcc467f89e58ab6ec6c8a3b03e99d862025607e
MinoruKi <124002390+MinoruKi@users.noreply.github.com>
2023-03-02 18:00:29 +0000

    Created a more accurate unit test script
    Created a more accurate bone structure for the unit tests and removed unnecessary file

--- services/tests/test_payments.py
+++ services/tests/test_payments.py
@@ -1,9 +1,38 @@
-from flask import Flask
+import unittest
+from payments.payments import app
 
-from services.payments.payments import get_index
+#Creating a class in order to test createCheckout()
+class MakePurchasableTest(unittest.TestCase):
+    
+    def MakePurchasable_test(self):
+        a = 1
 
-def test_base_route():
-    app = Flask(__name__)
-    get_index(app)
-    client = app.test_client()
-    url = '/'
+#Creating a class in order to test addProductDatabase()
+class AddProductDatabaseTest(unittest.TestCase):
+    
+    def addProductDatabase_test(self):
+        a = 1
+
+#Creating a class in order to test createCheckout()
+class CreateCheckoutTest(unittest.TestCase):
+    
+    def createCheckout_test(self):
+        a = 1
+
+#Creating a class in order to test get_index()
+class GetIndexTest(unittest.TestCase):
+    
+    def get_index_test(self):
+        a = 1
+
+#Creating a class in order to test redirectCheckout()
+class RedirectCheckoutTest(unittest.TestCase):
+    
+    def redirectCheckout_test(self):
+        a = 1
+
+#Creating a class in order to test webhookReceived()
+class WebhookReceivedTest(unittest.TestCase):
+    
+    def webhookReceived_test(self):
+        a = 1
