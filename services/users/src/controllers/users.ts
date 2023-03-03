8e0d4ac87c3da855dab956961fe7d3287fc9b7aa
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-03 00:06:58 +0000

    Added routers for health

--- services/users/src/controllers/users.ts
+++ services/users/src/controllers/users.ts
@@ -7,3 +7,5 @@ async function main() {
 }
 
 main().catch(console.error);
+
+export default prisma;
---
+++ services/users/src/routers/users.ts
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import usersControllers from '../controllers/users';
+
+const authRouter: Router = Router();
+
+// usersRouter.post('/updateName', usersControllers.postLogin);
+
+export default authRouter;
