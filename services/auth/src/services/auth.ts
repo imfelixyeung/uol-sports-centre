a638355d0b6f411a1a77b63a8b5d690e15f81cf0
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-05 19:49:06 +0000

    refactor: add jsdoc

--- services/auth/src/services/auth.ts
+++ services/auth/src/services/auth.ts
@@ -5,6 +5,11 @@ import {UserRegistry} from '../persistence/users';
 import {Credentials, ResetPassword} from '../schema/credentials';
 import {JsonWebToken} from '../schema/jwt';
 
+/**
+ * signs a user in with their credentials
+ * @param credentials email password
+ * @returns jwt token and refresh token
+ */
 export const signInWithCredentials = async (credentials: Credentials) => {
   const {email, password} = credentials;
 
@@ -33,6 +38,11 @@ export const signInWithCredentials = async (credentials: Credentials) => {
   return {token, refreshToken};
 };
 
+/**
+ * registers a user with their credentials
+ * @param credentials email password
+ * @returns jwt token and refresh token
+ */
 export const registerWithCredentials = async (credentials: Credentials) => {
   const {email, password} = credentials;
 
@@ -59,6 +69,10 @@ export const registerWithCredentials = async (credentials: Credentials) => {
   return {token, refreshToken};
 };
 
+/**
+ * resets a user's password
+ * @param options old credentials and new password
+ */
 export const resetPassword = async (options: ResetPassword) => {
   const {email, password, newPassword} = options;
 
@@ -84,6 +98,11 @@ export const resetPassword = async (options: ResetPassword) => {
   await UserRegistry.updatePassword(user.id, newHashedPassword);
 };
 
+/**
+ * gets the jwt payload from a token, throws error if malformed
+ * @param token jwt token
+ * @returns session (decoded jwt payload)
+ */
 export const getSessionFromToken = async (token: JsonWebToken) => {
   try {
     const decoded = TokenRegistry.verifyToken(token);
@@ -93,12 +112,22 @@ export const getSessionFromToken = async (token: JsonWebToken) => {
   }
 };
 
+/**
+ * signs a user out by invalidating all tokens associated with the token
+ * @param token jwt token
+ */
 export const signOutToken = async (token: JsonWebToken) => {
   await TokenRegistry.invalidateToken(token);
 };
 
+/**
+ * refreshes an access token and refresh token
+ * @param token jwt token
+ * @param refreshToken  jwt refresh token
+ * @returns refreshed token and refresh token
+ */
 export const refreshAccessToken = async (
-  token: string,
+  token: JsonWebToken,
   refreshToken: string
 ) => {
   const verified = !!(
