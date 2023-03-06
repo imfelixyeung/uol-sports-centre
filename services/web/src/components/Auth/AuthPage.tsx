e5649e73e3fcaf938ae9fcdc58e68a55d0fb50fa
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-06 22:05:12 +0000

    feat: sliding auth box

--- services/web/src/components/Auth/AuthPage.tsx
+++ services/web/src/components/Auth/AuthPage.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import {motion} from 'framer-motion';
 import type {FC} from 'react';
 import type {AuthFormProps} from '~/components/Auth/AuthForm';
 import AuthForm from '~/components/Auth/AuthForm';
@@ -7,10 +9,16 @@ const AuthPage: FC<AuthFormProps> = props => {
   return (
     <div className="from-red-600 bg-gradient-to-br via-purple-600 to-blue-600 grow py-6 flex flex-col">
       <div className="grid md:grid-cols-2 container grow">
-        <div className="bg-black p-10 flex flex-col items-center justify-center gap-8">
+        <motion.div
+          layoutId="auth-page"
+          className={clsx(
+            'bg-black p-10 flex flex-col items-center justify-center gap-8',
+            props.variant === 'login' ? 'col-start-1' : 'col-start-2'
+          )}
+        >
           <AppIcon />
           <AuthForm {...props} />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
