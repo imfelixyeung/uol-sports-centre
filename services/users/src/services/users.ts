21c87a960b869977a49a83441bde68b0ec0da1c3
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-06 16:01:39 +0000

    Added skeletons

--- services/users/src/services/users.ts
+++ services/users/src/services/users.ts
@@ -2,6 +2,37 @@ function editMembership(userID: number) {
   return undefined;
 }
 
+function returnFullRecord(userID: number){
+  return undefined
+}
+
+function editFirstName(userID: number){
+  return undefined
+}
+
+function editSecondName(userID: number){
+  return undefined
+}
+
+function editAccountID(userID: number){
+  return undefined
+}
+
+function editPaymentID(userID: number){
+  return undefined
+}
+
+function 
+
 export function processData(data: String): String {
   return data;
 }
+
+// testing,
+// viewFullRecord,
+// updateFirstName,
+// updateSecondName,
+// updateAccountID,
+// updatePaymentID,
+// updateBookingID,
+// updateMembership,
