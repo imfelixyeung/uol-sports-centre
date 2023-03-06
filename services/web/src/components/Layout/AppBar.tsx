f3b09789da63d86f4eacd94ed50a61cd9de75d93
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-06 06:47:32 +0000

    feat: appbar

---
+++ services/web/src/components/Layout/AppBar.tsx
@@ -0,0 +1,37 @@
+import Button from '../Button';
+
+const AppBar = () => {
+  return (
+    <div className="bg-base-100">
+      <header className="flex justify-between items-center m-3">
+        <div className="flex items-center gap-3">
+          <div className="h-8 w-8 bg-base-content"></div>
+          <h1 className="uppercase font-black leading-4">
+            Sports
+            <br />
+            Centre
+          </h1>
+        </div>
+        <nav>
+          <ul className="flex gap-6 font-bold">
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Facilities</a>
+            </li>
+            <li>
+              <a href="">Pricing</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <Button intent="primary">Account</Button>
+      </header>
+    </div>
+  );
+};
+
+export default AppBar;
--- services/web/src/components/Layout/Layout.tsx
+++ services/web/src/components/Layout/Layout.tsx
@@ -1,46 +1,11 @@
-import clsx from 'clsx';
 import type {FC, PropsWithChildren} from 'react';
-import {useState} from 'react';
-import {useAuth} from '~/providers/auth/hooks/useAuth';
-import Drawer from './Drawer';
+import AppBar from './AppBar';
 
 const Layout: FC<PropsWithChildren> = ({children}) => {
-  const auth = useAuth();
-  const [drawerOpen, setDrawerOpen] = useState(false);
-
-  const toggleDrawer = () => setDrawerOpen(open => !open);
-
   return (
     <>
-      {auth.session && (
-        <aside
-          className={clsx(
-            'fixed md:max-w-[12rem] inset-x-0 md:w-full inset-y-0 bg-red-400 md:block',
-            drawerOpen ? '' : 'hidden'
-          )}
-        >
-          <Drawer onClosed={() => setDrawerOpen(false)} />
-        </aside>
-      )}
-      <div
-        className={clsx(
-          'flex flex-col min-h-screen',
-          auth.session && 'md:ml-[12rem]'
-        )}
-      >
-        <div className="bg-red-400 md:bg-transparent">
-          <header className="flex justify-between items-center m-3">
-            <h1 className="bg-gray-200 p-3">Sports Center Website</h1>
-            {auth.session && (
-              <button
-                className="p-3 bg-gray-300 md:hidden"
-                onClick={toggleDrawer}
-              >
-                Menu
-              </button>
-            )}
-          </header>
-        </div>
+      <div className="flex flex-col min-h-screen">
+        <AppBar />
         <main className="grow flex flex-col">{children}</main>
       </div>
     </>
