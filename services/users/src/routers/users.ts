bc86ae7b077038d0a80eec292f1c2c5f0e58b6bd
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-06 15:51:00 +0000

    Added view full record to routers

--- services/users/src/routers/users.ts
+++ services/users/src/routers/users.ts
@@ -4,5 +4,6 @@ import usersControllers from '../controllers/users';
 const userRouter: Router = Router();
 
 userRouter.get('/testing', usersControllers.testing);
+userRouter.get('/viewFullRecord', usersControllers.viewFullRecord);
 
 export default userRouter;
