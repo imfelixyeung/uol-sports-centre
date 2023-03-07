eb56a072ba2077da7aa8eb8550f088e48e414a26
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-07 22:02:36 +0000

    refactor: rename `report.ts` to `status.ts`

--- services/status/src/routers/status.ts
+++ services/status/src/routers/status.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express';
-import statusControllers from '../controllers/report';
+import statusControllers from '../controllers/status';
 
 const statusRouter: Router = express.Router();
 
