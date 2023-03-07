3dd57c952b88de7d8e41fbafe5c1754023e473c9
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-07 18:13:06 +0000

    feat: mobile responsive

--- services/web/src/pages/dashboard/index.tsx
+++ services/web/src/pages/dashboard/index.tsx
@@ -4,61 +4,81 @@ import Card from '~/components/Card';
 import Typography from '~/components/Typography';
 
 const UserDashboardPage = () => {
+  const navSection = (
+    <section className="my-16">
+      <div className="flex gap-3 justify-between flex-wrap">
+        <Card variant="default" title="Booking" grow />
+        <Card variant="alt" title="Memberships" grow />
+        <Card variant="red" title="Profile" grow />
+      </div>
+    </section>
+  );
+
+  const availableClassesSection = (
+    <section className="my-16">
+      <Typography as="h2" styledAs="h1" uppercase className="mb-8">
+        {'/// Available Classes'}
+      </Typography>
+      <div className="flex flex-wrap gap-3">
+        <Card variant="default" title="Card Title" grow />
+        <Card variant="default" title="Card Title" grow />
+        <Card variant="default" title="Card Title" grow />
+        <Card variant="default" title="Card Title" grow />
+        <Card variant="default" title="Card Title" grow />
+      </div>
+    </section>
+  );
+
+  const upcomingSection = (
+    <section className="my-16 flex flex-col gap-3">
+      <Typography as="h2" styledAs="h1" uppercase>
+        {'/// Upcoming'}
+      </Typography>
+      <BookingActivity
+        datetime={new Date('2023-01-01 00:00')}
+        title="Booking Facility"
+      />
+      <BookingActivity
+        datetime={new Date('2023-01-01 00:00')}
+        title="Booking Facility"
+      />
+      <BookingActivity
+        datetime={new Date('2023-01-01 00:00')}
+        title="Booking Facility"
+      />
+      <Button intent="primary" outline>
+        Button
+      </Button>
+    </section>
+  );
+
   return (
     <div>
       <header className="container my-16">
-        <Typography as="h1" styledAs="display2" uppercase>
+        <Typography as="h1" styledAs="display2" uppercase className="mb-3">
           {'Hello {first_name}!'}
         </Typography>
         <Typography as="p" styledAs="subtext" uppercase>
           Welcome to a sports centre
         </Typography>
       </header>
-      <div className="bg-white text-black">
-        <main className="container py-8 flex gap-3">
+      <main className="bg-white text-black">
+        {/* Desktop */}
+        <div className="container py-8 gap-3 hidden lg:flex">
           <div>
-            <section className="my-16">
-              <div className="flex gap-3 justify-between">
-                <Card variant="default" title="Booking" grow />
-                <Card variant="alt" title="Memberships" grow />
-                <Card variant="red" title="Profile" grow />
-              </div>
-            </section>
-            <section className="my-16">
-              <Typography as="h2" styledAs="h1" uppercase className="mb-8">
-                {'/// Available Classes'}
-              </Typography>
-              <div className="flex flex-wrap gap-3">
-                <Card variant="default" title="Card Title" grow />
-                <Card variant="default" title="Card Title" grow />
-                <Card variant="default" title="Card Title" grow />
-                <Card variant="default" title="Card Title" grow />
-                <Card variant="default" title="Card Title" grow />
-              </div>
-            </section>
+            {navSection}
+            {availableClassesSection}
           </div>
-          <section className="my-16 flex flex-col gap-3">
-            <Typography as="h2" styledAs="h2" uppercase>
-              {'/// Upcoming'}
-            </Typography>
-            <BookingActivity
-              datetime={new Date('2023-01-01 00:00')}
-              title="Booking Facility"
-            />
-            <BookingActivity
-              datetime={new Date('2023-01-01 00:00')}
-              title="Booking Facility"
-            />
-            <BookingActivity
-              datetime={new Date('2023-01-01 00:00')}
-              title="Booking Facility"
-            />
-            <Button intent="primary" outline>
-              Button
-            </Button>
-          </section>
-        </main>
-      </div>
+          {upcomingSection}
+        </div>
+
+        {/* Mobile */}
+        <div className="container py-8 flex-col gap-3 lg:hidden">
+          {navSection}
+          {upcomingSection}
+          {availableClassesSection}
+        </div>
+      </main>
     </div>
   );
 };
