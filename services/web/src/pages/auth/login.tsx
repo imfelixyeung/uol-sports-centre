fdce9491de1463da9d91ceca64b8a8adebf0dd03
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-07 18:42:06 +0000

    fix: nextjs hydration error

--- services/web/src/pages/auth/login.tsx
+++ services/web/src/pages/auth/login.tsx
@@ -1,8 +1,7 @@
 import AuthPage from '~/components/Auth/AuthPage';
+import type {NextPageWithLayout} from '~/types/NextPage';
 
-const LoginPage = AuthPage;
-LoginPage.defaultProps = {
-  variant: 'login',
-};
+const LoginPage: NextPageWithLayout = () => <AuthPage variant="login" />;
+LoginPage.getLayout = AuthPage.getLayout;
 
 export default LoginPage;
--- services/web/src/pages/auth/register.tsx
+++ services/web/src/pages/auth/register.tsx
@@ -1,8 +1,7 @@
 import AuthPage from '~/components/Auth/AuthPage';
+import type {NextPageWithLayout} from '~/types/NextPage';
 
-const RegisterPage = AuthPage;
-RegisterPage.defaultProps = {
-  variant: 'register',
-};
+const RegisterPage: NextPageWithLayout = () => <AuthPage variant="register" />;
+RegisterPage.getLayout = AuthPage.getLayout;
 
 export default RegisterPage;
