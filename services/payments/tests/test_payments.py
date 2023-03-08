da97326203ba55e556b48ae1c4745365c9780290
MinoruKi <124002390+MinoruKi@users.noreply.github.com>
2023-03-08 16:30:14 +0000

    Added testing for Checkout(). However, need to check connection to database to make it work.

--- services/payments/tests/test_payments.py
+++ services/payments/tests/test_payments.py
@@ -4,6 +4,7 @@ import stripe
 
 import sys
 from pathlib import Path
+from unittest.mock import Mock, patch
 
 sys.path[0] = str(Path(sys.path[0]).parent)
 
@@ -11,14 +12,22 @@ import payments
 from payments import app
 from payments import MakePurchasable
 from payments import addProductDatabase
+from payments import createCheckout
 
 def create_testDatabase():
     connection = sqlite3.connect("database.db")
-    with open('services/payments/paymentSchema.sql') as schema:
+    with open('paymentTestSchema.sql') as schema:
       connection.executescript(schema.read())
     connection.close()
 
 class TestingPaymentsMicroservice(unittest.TestCase):
+  def setUp(self):
+     app.testing = True
+     self.client = app.test_client()
+     self.product_name = "Sports Centre Membership"
+     self.product_price = 50.00
+     self.user_id = 467468
+
   #test createCheckout()
   def test_MakePurchasable_test(self):
     #tests if it can make a test product purchasable
@@ -45,6 +54,7 @@ class TestingPaymentsMicroservice(unittest.TestCase):
     #stripe.Product.delete(productStripe.id)
 
   #test addProductDatabase()
+
   def test_addProductDatabase(self):
     
     #Add products using payments
@@ -67,8 +77,11 @@ class TestingPaymentsMicroservice(unittest.TestCase):
     self.assertEqual(t2, "subscription-test")
 
   #test createCheckout()
-  def createCheckout_test(self):
-    a = 1
+  @patch("stripe.checkout.Session.create")
+  def test_create_checkout_success(self, mock_checkout_session_create):
+    mock_checkout_session_create.return_value = Mock(url='http://localhost:5000/checkout-session')
+    session_url = createCheckout('stripe_customer_id', self.product_name)
+    self.assertEqual(session_url, 'http://localhost:5000/checkout-session')
 
   #test get_index()
   def get_index_test(self):
