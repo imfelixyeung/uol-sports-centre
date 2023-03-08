b2be13dfd0879c8744d40ef72ce92ffa893c5e2f
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-08 23:55:07 +0000

    fix: text styles

--- services/web/src/components/LandingPage/Memberships.tsx
+++ services/web/src/components/LandingPage/Memberships.tsx
@@ -56,12 +56,12 @@ const MembershipCard: FC<MembershipCardProps> = ({membership}) => {
           'bg-cover bg-center'
         )}
       >
-        <Typography as="p" styledAs="h2" uppercase>
+        <Typography as="p" styledAs="h1" uppercase>
           {name}
         </Typography>
         {bestValue && (
-          <div className="absolute top-6 right-0 py-2 px-4 bg-white">
-            <Typography as="p" styledAs="h2">
+          <div className="absolute top-6 right-0 py-2 px-4 bg-black text-primary">
+            <Typography as="p" styledAs="button">
               Best Value
             </Typography>
           </div>
