7b816dd4d1c61369794c7f96000c250cd4deed94
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-12 23:23:18 +0000

    feat: icons demo page

--- services/web/src/pages/design-system/index.tsx
+++ services/web/src/pages/design-system/index.tsx
@@ -3,24 +3,36 @@ import {buttonStyles} from '~/components/Button';
 import Seo from '~/components/Seo';
 import Typography from '~/components/Typography';
 
+const links = [
+  {
+    href: '/design-system/components',
+    label: 'Components',
+  },
+  {
+    href: '/design-system/style-guide',
+    label: 'Style Guide',
+  },
+  {
+    href: '/design-system/icons',
+    label: 'Icons',
+  },
+];
+
 const DesignSystemPage = () => {
   return (
     <>
       <Seo title="Design System" />
       <div className="container my-16 grid gap-3 text-center">
         <Typography.h1>Design System</Typography.h1>
-        <Link
-          className={buttonStyles({intent: 'primary'})}
-          href="/design-system/components"
-        >
-          Components
-        </Link>
-        <Link
-          className={buttonStyles({intent: 'primary'})}
-          href="/design-system/style-guide"
-        >
-          Style Guide
-        </Link>
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            className={buttonStyles({intent: 'primary'})}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
     </>
   );
