7f6df25dddeac20d3d03494ff634444446d8b3d6
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-13 11:36:29 +0000

    Updated controllers

--- services/users/src/controllers/users.ts
+++ services/users/src/controllers/users.ts
@@ -1,5 +1,15 @@
 import {Request, Response} from 'express';
-import {processData} from '../services/users';
+import {
+  createNewUser,
+  editAccountID,
+  editBookingID,
+  editFirstName,
+  editMembership,
+  editPaymentID,
+  editSecondName,
+  processData,
+  returnFullRecord,
+} from '../services/users';
 
 async function testing(req: Request, res: Response) {
   const finishedData: String = await processData(req.query.trtr as string);
@@ -11,74 +21,61 @@ async function testing(req: Request, res: Response) {
 }
 
 async function viewFullRecord(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await returnFullRecord(req.query.id as number),
   });
 }
 
 async function updateMembership(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  // need to extract the data from the request
+  // we need to create a valid UserDBA object
+  // we need to pass that object to the editMembership function
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await editMembership(),
   });
 }
 
 async function updateFirstName(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await editFirstName(req.query.id as number),
   });
 }
 
 async function updateSecondName(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await editSecondName(req.query.id as number),
   });
 }
 
 async function updateAccountID(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await editAccountID(req.query.id as number),
   });
 }
 
 async function updatePaymentID(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await editPaymentID(req.query.id as number),
   });
 }
 
 async function updateBookingID(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await editBookingID(req.query.id as number),
   });
 }
 
 async function createUser(req: Request, res: Response) {
-  const finishedData: String = await processData(req.query.trtr as string);
-  const otherData: String = 'Test';
-  return res.json({
-    data: finishedData,
-    data2: otherData,
+  return res.status(200).send({
+    status: 'OK',
+    bookings: await createNewUser(req.query.id as number),
   });
 }
 
