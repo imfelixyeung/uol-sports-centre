4fe58f8cfaf2c4cf1b3ce13cfa2055112f26754d
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-13 12:19:14 +0000

    Created new user controller

--- services/users/src/controllers/users.ts
+++ services/users/src/controllers/users.ts
@@ -1,4 +1,6 @@
 import {Request, Response} from 'express';
+import express from 'express';
+import {z} from 'zod';
 import {
   createNewUser,
   deleteExistingUser,
@@ -10,6 +12,7 @@ import {
   processData,
   returnFullRecord,
 } from '../services/users';
+import {CreateUserDBA} from '../services/dbRequests';
 
 async function testing(req: Request, res: Response) {
   const finishedData: String = await processData(req.query.trtr as string);
@@ -65,10 +68,36 @@ async function updatePaymentID(req: Request, res: Response) {
   });
 }
 
-async function createUser(req: Request, res: Response) {
+async function createUser(req: express.Request, res: express.Response) {
+  const createUserSchema = z.object({
+    id: z.number(),
+    accountID: z.number(),
+    paymentID: z.number(),
+    firstName: z.string(),
+    lastName: z.string(),
+    membership: z.string(),
+  });
+  const body = createUserSchema.safeParse(req.body);
+  if (!body.success)
+    return res.status(400).json({
+      status: 'error',
+      message: 'malformed parameters',
+      error: body.error,
+    });
+
+  const userData: CreateUserDBA = body.data;
+  const newUser = await createNewUser(userData);
+
+  if (newUser === null)
+    return res.status(500).send({
+      status: 'error',
+      message: 'Unable to create user',
+    });
+
+  // after passing all the above checks, the booking should be okay
   return res.status(200).send({
     status: 'OK',
-    bookings: await createNewUser(req.query.id as number),
+    booking: newUser,
   });
 }
 
