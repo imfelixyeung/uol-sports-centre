184fd93ddcd91c24a3a29f23426b4982131ba603
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-14 01:34:59 +0000

    feat: more options

--- services/web/src/components/Auth/hooks/useRedirectTo.tsx
+++ services/web/src/components/Auth/hooks/useRedirectTo.tsx
@@ -1,13 +1,29 @@
 import {useRouter} from 'next/router';
 import {useMemo} from 'react';
 
+interface UseRedirectToOptions {
+  /**
+   * the default url to redirect to if unable to resolve from url query
+   * @default '/'
+   */
+  defaultRedirectTo?: string;
+
+  /**
+   * indicates whether to allow cross origin redirects
+   * @default false
+   */
+  allowCrossOriginRedirect?: boolean;
+}
+
 /**
  * Resolves where to redirect after user authentication.
  * @returns the url to redirect to
  */
-const useRedirectTo = () => {
+const useRedirectTo = (options: UseRedirectToOptions = {}) => {
+  const {allowCrossOriginRedirect = false, defaultRedirectTo = '/'} = options;
+
   const router = useRouter();
-  const redirectTo = useMemo(() => {
+  const rawRedirectTo = useMemo(() => {
     const redirectTo = router.query.redirect;
 
     if (redirectTo === undefined) return null;
@@ -17,7 +33,18 @@ const useRedirectTo = () => {
     return redirectTo[0];
   }, [router.query.redirect]);
 
-  return redirectTo ?? '/';
+  const sanitisedRedirectTo = useMemo(() => {
+    if (allowCrossOriginRedirect) return rawRedirectTo;
+
+    const currentOrigin = window.location.origin;
+    const redirectToOrigin = new URL(rawRedirectTo ?? '', currentOrigin).origin;
+
+    if (redirectToOrigin !== currentOrigin) return null;
+
+    return rawRedirectTo;
+  }, [rawRedirectTo, allowCrossOriginRedirect]);
+
+  return sanitisedRedirectTo ?? defaultRedirectTo;
 };
 
 export default useRedirectTo;
