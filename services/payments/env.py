f341d5a96727a750cca53c0f0781ccbacf4b9304
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-15 19:50:14 +0000

    ci: add type checking

--- services/payments/env.py
+++ services/payments/env.py
@@ -14,7 +14,7 @@ dirtoenv = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(dirtoenv, ".env"))
 
 STRIPE_API_KEY = os.getenv("STRIPE_API")
-APP_PORT = os.getenv("APP_PORT")
+APP_PORT_STRING = os.getenv("APP_PORT")
 
 # Docker defaults unset environment variables to empty strings
 # Will need to check if they are not empty strings on top of None
@@ -23,12 +23,12 @@ if not STRIPE_API_KEY or STRIPE_API_KEY == "":
     print("Error: 'STRIPE_API' environment variable is not set")
     sys.exit(1)
 
-if not APP_PORT or APP_PORT == "":
+if not APP_PORT_STRING or APP_PORT_STRING == "":
     print("Error: 'APP_PORT' environment variable is not set")
     sys.exit(1)
 
 try:
-    APP_PORT = int(APP_PORT)
+    APP_PORT = int(APP_PORT_STRING)
 except ValueError:
     print("APP_PORT environment variable is not an integer")
     sys.exit(1)
