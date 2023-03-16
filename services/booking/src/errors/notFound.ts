80fd665e660344743092e31c5ebeba4b28a1c865
William Neild <wneild5@gmail.com>
2023-03-16 21:08:03 +0000

    refactor: clean up dead code

--- services/booking/src/errors/notFound.ts
+++ services/booking/src/errors/notFound.ts
@@ -1,8 +1,5 @@
 export default class NotFoundError extends Error {
   constructor(msg: string) {
     super(msg);
-
-    // // Set the prototype explicitly.
-    // Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 }
