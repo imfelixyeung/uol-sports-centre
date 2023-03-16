62790051a7a5cceb58d2faf1d031a46e81c3da9f
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-16 21:35:07 +0000

    refactor: remove outmost fragment

--- services/web/src/components/Layout/Layout.tsx
+++ services/web/src/components/Layout/Layout.tsx
@@ -4,13 +4,11 @@ import Footer from './Footer';
 
 const Layout: FC<PropsWithChildren> = ({children}) => {
   return (
-    <>
-      <div className="flex min-h-screen flex-col">
-        <AppBar />
-        <main className="flex grow flex-col">{children}</main>
-        <Footer />
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col">
+      <AppBar />
+      <main className="flex grow flex-col">{children}</main>
+      <Footer />
+    </div>
   );
 };
 
