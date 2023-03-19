70514d04ef819dd5e7b4179d68b90b1daae85216
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-19 17:55:04 +0000

    feat: add page studs

---
+++ services/web/src/pages/pricing.tsx
@@ -0,0 +1,12 @@
+import PageHero from '~/components/PageHero';
+
+const PricingPage = () => {
+  // TODO: Populate Page
+  return (
+    <>
+      <PageHero title="Pricing" />
+    </>
+  );
+};
+
+export default PricingPage;
