d7076c5b67a736a6a2c667ab3424b24c1b7cc0d2
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-23 21:22:39 +0000

    feat: basket partially follows hifi design

---
+++ services/web/src/components/BookingFilterForm.tsx
@@ -0,0 +1,98 @@
+import {Field, Form, Formik} from 'formik';
+import type {FC} from 'react';
+import {
+  useGetFacilitiesQuery,
+  useGetFacilityActivitiesQuery,
+} from '~/redux/services/api';
+import type {BookingAvailabilityRequest} from '~/redux/services/types/bookings';
+import FormikAutoSubmit from './FormikAutoSubmit';
+
+const BookingFilterForm: FC<{
+  onFilterChange: (
+    filter: Omit<BookingAvailabilityRequest, 'limit' | 'page'>
+  ) => void;
+}> = ({onFilterChange}) => {
+  const facilitiesData = useGetFacilitiesQuery();
+  const activitiesData = useGetFacilityActivitiesQuery();
+
+  const facilities = facilitiesData.data;
+  const activities = activitiesData.data;
+
+  if (!facilities || !activities) return null; // TODO: handle loading, error states
+
+  const initialValues: {
+    start?: string;
+    end?: string;
+    activity?: number;
+    facility?: number;
+  } = {};
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        onFilterChange({
+          start: values.start ? new Date(values.start).getTime() : undefined,
+          end: values.end ? new Date(values.end).getTime() : undefined,
+          activityId: values.activity,
+          facilityId: values.facility,
+        });
+        actions.setSubmitting(false);
+      }}
+    >
+      <Form className="mb-8 grid grid-cols-2 gap-3 bg-black p-8 text-white lg:grid-cols-4">
+        <FormikAutoSubmit />
+        <label className="flex grow flex-col">
+          <span>From</span>
+          <Field
+            name="start"
+            type="datetime-local"
+            className="p-2 text-black"
+          />
+        </label>
+        <label className="flex grow flex-col">
+          <span>To</span>
+          <Field name="end" type="datetime-local" className="p-2 text-black" />
+        </label>
+        <label className="flex grow flex-col">
+          <span>Activity</span>
+          <Field
+            name="activity"
+            as="select"
+            className="p-2 text-black"
+            defaultValue=""
+          >
+            <option value="" hidden>
+              ------
+            </option>
+            {activities.map(activity => (
+              <option key={activity.id} value={activity.id}>
+                {activity.name}
+              </option>
+            ))}
+          </Field>
+        </label>
+        <label className="flex grow flex-col">
+          <span>Facility</span>
+          <Field
+            name="facility"
+            as="select"
+            className="p-2 text-black"
+            defaultValue=""
+          >
+            <option value="" hidden>
+              ------
+            </option>
+            {facilities.map(facility => (
+              <option key={facility.id} value={facility.id}>
+                {facility.name}
+              </option>
+            ))}
+          </Field>
+        </label>
+      </Form>
+    </Formik>
+  );
+};
+
+export default BookingFilterForm;
--- services/web/src/pages/dashboard/bookings/new.tsx
+++ services/web/src/pages/dashboard/bookings/new.tsx
@@ -1,21 +1,12 @@
-import {Field, Form, Formik} from 'formik';
-import type {FC} from 'react';
 import {useState} from 'react';
-import BookingActivity from '~/components/BookingActivity';
+import BookingFilterForm from '~/components/BookingFilterForm';
 import Bookings from '~/components/Bookings';
-import Button from '~/components/Button';
-import FormikAutoSubmit from '~/components/FormikAutoSubmit';
 import PageHero from '~/components/PageHero';
+import SelectedBookingsDropdown from '~/components/SelectedBookingsDropdown';
 import Seo from '~/components/Seo';
 import {withPageAuthRequired} from '~/providers/auth';
 import {withUserOnboardingRequired} from '~/providers/user';
-import {removeBooking, selectBookings} from '~/redux/features/basket';
-import {useAppDispatch, useAppSelector} from '~/redux/hooks';
-import {
-  useGetAvailableBookingsQuery,
-  useGetFacilitiesQuery,
-  useGetFacilityActivitiesQuery,
-} from '~/redux/services/api';
+import {useGetAvailableBookingsQuery} from '~/redux/services/api';
 import type {BookingAvailabilityRequest} from '~/redux/services/types/bookings';
 
 const NewBookingsPage = () => {
@@ -56,119 +47,3 @@ const NewBookingsPage = () => {
 export default withPageAuthRequired(
   withUserOnboardingRequired(NewBookingsPage)
 );
-
-const BookingFilterForm: FC<{
-  onFilterChange: (
-    filter: Omit<BookingAvailabilityRequest, 'limit' | 'page'>
-  ) => void;
-}> = ({onFilterChange}) => {
-  const facilitiesData = useGetFacilitiesQuery();
-  const activitiesData = useGetFacilityActivitiesQuery();
-
-  const facilities = facilitiesData.data;
-  const activities = activitiesData.data;
-
-  if (!facilities || !activities) return null; // TODO: handle loading, error states
-
-  const initialValues: {
-    start?: string;
-    end?: string;
-    activity?: number;
-    facility?: number;
-  } = {};
-
-  return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values, actions) => {
-        onFilterChange({
-          start: values.start ? new Date(values.start).getTime() : undefined,
-          end: values.end ? new Date(values.end).getTime() : undefined,
-          activityId: values.activity,
-          facilityId: values.facility,
-        });
-        actions.setSubmitting(false);
-      }}
-    >
-      <Form className="mb-8 grid grid-cols-2 gap-3 bg-black p-8 text-white lg:grid-cols-4">
-        <FormikAutoSubmit />
-        <label className="flex grow flex-col">
-          <span>From</span>
-          <Field
-            name="start"
-            type="datetime-local"
-            className="p-2 text-black"
-          />
-        </label>
-        <label className="flex grow flex-col">
-          <span>To</span>
-          <Field name="end" type="datetime-local" className="p-2 text-black" />
-        </label>
-        <label className="flex grow flex-col">
-          <span>Activity</span>
-          <Field
-            name="activity"
-            as="select"
-            className="p-2 text-black"
-            defaultValue=""
-          >
-            <option value="" hidden>
-              ------
-            </option>
-            {activities.map(activity => (
-              <option key={activity.id} value={activity.id}>
-                {activity.name}
-              </option>
-            ))}
-          </Field>
-        </label>
-        <label className="flex grow flex-col">
-          <span>Facility</span>
-          <Field
-            name="facility"
-            as="select"
-            className="p-2 text-black"
-            defaultValue=""
-          >
-            <option value="" hidden>
-              ------
-            </option>
-            {facilities.map(facility => (
-              <option key={facility.id} value={facility.id}>
-                {facility.name}
-              </option>
-            ))}
-          </Field>
-        </label>
-      </Form>
-    </Formik>
-  );
-};
-
-const SelectedBookingsDropdown: FC = () => {
-  const bookings = useAppSelector(selectBookings);
-  const dispatch = useAppDispatch();
-  return (
-    <>
-      {bookings.map((booking, index) => (
-        <BookingActivity
-          key={index}
-          datetime={new Date(booking.starts)}
-          capacity={booking.capacity}
-          duration={booking.duration}
-          name={booking.event.name}
-          facility="Facility"
-          variant="tile"
-          action={
-            <Button
-              intent="secondary"
-              onClick={() => dispatch(removeBooking(booking))}
-            >
-              Remove
-            </Button>
-          }
-        />
-      ))}
-    </>
-  );
-};
