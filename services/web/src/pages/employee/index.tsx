e8acffb7c7c7bd6ae5e7acb99f7458afa27d2946
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-24 11:14:25 +0000

    feat: employee booking form

--- services/web/src/pages/employee/index.tsx
+++ services/web/src/pages/employee/index.tsx
@@ -1,15 +1,21 @@
+import {useState} from 'react';
+import BookingFilterForm from '~/components/BookingFilterForm';
+import Bookings from '~/components/Bookings';
 import PageHero from '~/components/PageHero';
+import SelectedBookingsDropdown from '~/components/SelectedBookingsDropdown';
 import Typography from '~/components/Typography';
 import {withPageAuthRequired} from '~/providers/auth';
 import {withUserOnboardingRequired} from '~/providers/user';
+import {useGetAvailableBookingsQuery} from '~/redux/services/api';
+import type {BookingAvailabilityRequest} from '~/redux/services/types/bookings';
 
 const EmployeePage = () => {
   return (
     <div>
       <PageHero title="Employee Dashboard" />
-      <section className="container">
+      <section className="container py-8">
         <Typography.h2>Create booking for customer</Typography.h2>
-        <form action="">Form</form>
+        <CreateBookingForm />
         <Typography.h3>View/Amend booking for customer</Typography.h3>
         <form action="">Form</form>
       </section>
@@ -20,3 +26,26 @@ const EmployeePage = () => {
 export default withPageAuthRequired(withUserOnboardingRequired(EmployeePage), {
   rolesAllowed: ['ADMIN', 'MANAGER', 'EMPLOYEE'],
 });
+
+const CreateBookingForm = () => {
+  const [filter, setFilter] = useState<BookingAvailabilityRequest>({});
+  const availableBookingsData = useGetAvailableBookingsQuery(filter);
+  const availableBookings = availableBookingsData.data?.availableBookings;
+
+  if (!availableBookings) return null; // TODO: handle loading, error states
+  return (
+    <div className="bg-white p-3 text-black">
+      <BookingFilterForm onFilterChange={setFilter} />
+      <Bookings
+        bookings={availableBookings.map(booking => ({
+          datetime: new Date(booking.starts),
+          capacity: booking.capacity,
+          duration: booking.duration,
+          name: booking.event.name,
+          availableBooking: booking,
+        }))}
+        title={<SelectedBookingsDropdown />}
+      />
+    </div>
+  );
+};
