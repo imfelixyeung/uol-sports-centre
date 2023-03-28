f982505ed70ddf472cfc56915b763f58d5a5d9bd
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-28 14:41:15 +0100

    Added remaining tests for editors and getters

--- services/users/src/services/__tests__/users.test.ts
+++ services/users/src/services/__tests__/users.test.ts
@@ -1,6 +1,14 @@
 import {dbMock} from '../../utils/__mocks__/db.mock';
 
-import {createNewUser, deleteExistingUser, editPaymentID} from '../users';
+import {
+  createNewUser,
+  deleteExistingUser,
+  editFirstName,
+  editMembership,
+  editPaymentID,
+  editSurname,
+  returnFullRecord,
+} from '../users';
 import {CreateUserDBA, EditUserDBA} from '../dbRequests';
 import {User} from '@prisma/client';
 
@@ -168,5 +176,199 @@ describe('editPaymentID', () => {
 
     // checks if the function returned the user that was edited
     expect(result).toEqual({id: editUserData.id});
+  });
+});
+
+// Test for editSurname function
+// It edits the surname of a user in the database
+// and then edits a user that does not exist
+// to test if the function throws an error
+describe('editSurname', () => {
+  editSurname;
+
+  // dummy data for testing
+  const editUserData: EditUserDBA = {
+    id: 0,
+    lastName: 'Ipsum',
+  };
+
+  it('should throw error if user does not exist', async () => {
+    // mocks the database to return null
+    dbMock.user.findUnique.mockResolvedValue(null);
+
+    // catches the thrown error
+    const result: Error = await editSurname(editUserData).catch(error => error);
+
+    // checks if the function threw an error and has the correct error message
+    expect(result instanceof Error).toBe(true);
+    expect(result.message).toBe(
+      `Error editing surname of user\nError: User with ID: ${editUserData.id} does not exist in database.`
+    );
+  });
+});
+
+// Test for editSurname function
+// It edits the surname of a user in the database
+// It should return the user that was edited successfully
+describe('editSurname', () => {
+  editSurname;
+
+  // dummy data for testing
+  const editUserData: EditUserDBA = {
+    id: 0,
+    lastName: 'Ipsum',
+  };
+
+  it('should return the edited user', async () => {
+    // mocks the database to return a user
+    dbMock.user.findUnique.mockResolvedValue({id: editUserData.id} as User);
+
+    // mocks the database to return the user that was edited
+    dbMock.user.update.mockResolvedValue({id: editUserData.id} as User);
+
+    // calls the function
+    const result = await editSurname(editUserData);
+
+    // checks if the function returned the user that was edited
+    expect(result).toEqual({id: editUserData.id});
+  });
+});
+
+// Test for editFirstName function
+// It edits the first name of a user in the database
+// and then edits a user that does not exist
+// to test if the function throws an error
+describe('editFirstName', () => {
+  editFirstName;
+
+  // dummy data for testing
+  const editUserData: EditUserDBA = {
+    id: 0,
+    firstName: 'Lorem',
+  };
+
+  it('should throw error if user does not exist', async () => {
+    // mocks the database to return null
+    dbMock.user.findUnique.mockResolvedValue(null);
+
+    // catches the thrown error
+    const result: Error = await editFirstName(editUserData).catch(
+      error => error
+    );
+
+    // checks if the function threw an error and has the correct error message
+    expect(result instanceof Error).toBe(true);
+    expect(result.message).toBe(
+      `Error editing first name of user\nError: User with ID: ${editUserData.id} does not exist in database.`
+    );
+  });
+});
+
+// Test for editFirstName function
+// It edits the first name of a user in the database
+// It should return the user that was edited successfully
+describe('editFirstName', () => {
+  editFirstName;
+
+  // dummy data for testing
+  const editUserData: EditUserDBA = {
+    id: 0,
+    firstName: 'Lorem',
+  };
+
+  it('should return the edited user', async () => {
+    // mocks the database to return a user
+    dbMock.user.findUnique.mockResolvedValue({id: editUserData.id} as User);
+
+    // mocks the database to return the user that was edited
+    dbMock.user.update.mockResolvedValue({id: editUserData.id} as User);
+
+    // calls the function
+    const result = await editFirstName(editUserData);
+
+    // checks if the function returned the user that was edited
+    expect(result).toEqual({id: editUserData.id});
+  });
+});
+
+// Test for editMembership function
+// It edits the membership of a user in the database
+// and then edits a user that does not exist
+// to test if the function throws an error
+describe('editMembership', () => {
+  editMembership;
+
+  // dummy data for testing
+  const editUserData: EditUserDBA = {
+    id: 0,
+    membership: 'Basic',
+  };
+
+  it('should throw error if user does not exist', async () => {
+    // mocks the database to return null
+    dbMock.user.findUnique.mockResolvedValue(null);
+
+    // catches the thrown error
+    const result: Error = await editMembership(editUserData).catch(
+      error => error
+    );
+
+    // checks if the function threw an error and has the correct error message
+    expect(result instanceof Error).toBe(true);
+    expect(result.message).toBe(
+      `Error editing membership of user\nError: User with ID: ${editUserData.id} does not exist in database.`
+    );
+  });
+});
+
+// Test for editMembership function
+// It edits the membership of a user in the database
+// It should return the user that was edited successfully
+describe('editMembership', () => {
+  editMembership;
+
+  // dummy data for testing
+  const editUserData: EditUserDBA = {
+    id: 0,
+    membership: 'Basic',
+  };
+
+  it('should return the edited user', async () => {
+    // mocks the database to return a user
+    dbMock.user.findUnique.mockResolvedValue({id: editUserData.id} as User);
+
+    // mocks the database to return the user that was edited
+    dbMock.user.update.mockResolvedValue({id: editUserData.id} as User);
+
+    // calls the function
+    const result = await editMembership(editUserData);
+
+    // checks if the function returned the user that was edited
+    expect(result).toEqual({id: editUserData.id});
+  });
+});
+
+// Test for returnFullRecord function
+// It returns the full record of a user in the database
+// and then returns a user that does not exist
+// to test if the function throws an error
+describe('returnFullRecord', () => {
+  returnFullRecord;
+
+  // dummy data for testing
+  const userID = 0;
+
+  it('should throw error if user does not exist', async () => {
+    // mocks the database to return null
+    dbMock.user.findUnique.mockResolvedValue(null);
+
+    // catches the thrown error
+    const result: Error = await returnFullRecord(userID).catch(error => error);
+
+    // checks if the function threw an error and has the correct error message
+    expect(result instanceof Error).toBe(true);
+    expect(result.message).toBe(
+      `Error returning full record of user\nError: User with ID: ${userID} does not exist in database.`
+    );
   });
 });
