99b1add721f415008e5418c5336cd4955825d335
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-28 14:29:08 +0100

    Added deleteExistingUser tests

--- services/users/src/services/__tests__/users.test.ts
+++ services/users/src/services/__tests__/users.test.ts
@@ -62,3 +62,56 @@ describe('createNewUser', () => {
     expect(result).toEqual(createUserData);
   });
 });
+
+// Test for deleteExistingUser function
+// It deletes a user from the database
+// and then deletes a user that does not exist
+// to test if the function throws an error
+describe('deleteExistingUser', () => {
+  deleteExistingUser;
+
+  // dummy data for testing
+  const userID = 0;
+
+  it('should throw error if user does not exist', async () => {
+    // mocks the database to return null
+    dbMock.user.findUnique.mockResolvedValue(null);
+
+    // catches the thrown error
+    const result: Error = await deleteExistingUser(userID).catch(
+      error => error
+    );
+
+    // checks if the function threw an error and has the correct error message
+    expect(result instanceof Error).toBe(true);
+    expect(result.message).toBe(
+      `Error deleting user\nError: User with ID: ${userID} does not exist in database.`
+    );
+  });
+});
+
+// Test for deleteExistingUser function
+// It deletes a user from the database
+// It should return the user that was deleted successfully
+describe('deleteExistingUser', () => {
+  deleteExistingUser;
+
+  // dummy data for testing
+  const userID = 0;
+
+  it('should return the deleted user', async () => {
+    // mocks the database to return a user
+    dbMock.user.findUnique.mockResolvedValue({id: userID} as User);
+
+    // mocks the database to return the user that was deleted
+    dbMock.user.delete.mockResolvedValue({id: userID} as User);
+
+    // calls the function
+    const result = await deleteExistingUser(userID);
+
+    // checks if the function returned the user that was deleted
+    expect(result).toEqual({id: userID});
+  });
+});
+
+// T
