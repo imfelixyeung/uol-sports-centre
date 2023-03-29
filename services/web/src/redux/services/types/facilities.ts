fefdf1a809b5a1c554e7d73d028b1825ff9c979d
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-29 01:51:58 +0100

    fix: made id required

--- services/web/src/redux/services/types/facilities.ts
+++ services/web/src/redux/services/types/facilities.ts
@@ -33,7 +33,8 @@ export type FacilityActivityResponse = FacilityActivity;
 export type FacilityTimesResponse = FacilityTime[];
 export type FacilityTimeResponse = FacilityTime;
 
-export type FacilityTimeRequest = Partial<FacilityTime>;
+export type FacilityTimeRequest = Partial<FacilityTime> &
+  Pick<FacilityTime, 'id'>;
 
 export type CreateFacilityRequest = Omit<Facility, 'id'>;
 export type CreateFacilityResponse = Facility;
