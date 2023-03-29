c988c31be095cf9083c2e754d6c32d25216ddb66
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-03-29 11:50:11 +0100

    Updated tests to work locally

--- services/users/src/env/index.ts
+++ services/users/src/env/index.ts
@@ -1,6 +1,9 @@
 // This file is used to parse environment variables
 
 import {z} from 'zod';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const envSchema = z.object({
   PORT: z.string().transform((port: string) => parseInt(port)),
@@ -12,7 +15,7 @@ export const safeEnv = envSchema.safeParse(process.env);
 
 if (!safeEnv.success) {
   console.log('Error while parsing env', safeEnv.error);
-  throw new Error('Failed to parse env');
+  throw new Error(`Failed to parse env ${safeEnv.error}`);
 }
 
 export const env = safeEnv.data;
