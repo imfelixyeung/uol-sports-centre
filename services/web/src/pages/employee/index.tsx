d6e6130f4245ff935ba1af98908a279cb2d4b2a5
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-03-29 11:04:59 +0100

    feat: selecting user

--- services/web/src/pages/employee/index.tsx
+++ services/web/src/pages/employee/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import {useState} from 'react';
+import {toast} from 'react-hot-toast';
 import BookingFilterForm from '~/components/BookingFilterForm';
 import Bookings from '~/components/Bookings';
 import {buttonStyles} from '~/components/Button';
@@ -8,10 +9,20 @@ import SelectedBookingsDropdown from '~/components/SelectedBookingsDropdown';
 import Typography from '~/components/Typography';
 import {withPageAuthRequired} from '~/providers/auth';
 import {withUserOnboardingRequired} from '~/providers/user';
-import {useGetAvailableBookingsQuery} from '~/redux/services/api';
+import {selectBookings} from '~/redux/features/basket';
+import {useAppDispatch, useAppSelector} from '~/redux/hooks';
+import {
+  useGetAvailableBookingsQuery,
+  useGetUserRecordQuery,
+} from '~/redux/services/api';
 import type {BookingAvailabilityRequest} from '~/redux/services/types/bookings';
 
 const EmployeePage = () => {
+  const [userIdSelected, setUserIdSelected] = useState<number | null>(null);
+  const userData = useGetUserRecordQuery(userIdSelected!, {
+    skip: userIdSelected === null,
+  });
+
   return (
     <div>
       <PageHero title="Employee Dashboard" />
@@ -22,10 +33,25 @@ const EmployeePage = () => {
         Check in user
       </Link>
       <section className="container py-8">
-        <Typography.h2>Create booking for customer</Typography.h2>
-        <CreateBookingForm />
-        <Typography.h3>View/Amend booking for customer</Typography.h3>
-        <form action="">Form</form>
+        <Typography.h2>User Id, Enter user id to continue</Typography.h2>
+        <label>
+          <span>User Id</span>
+          <input
+            type="number"
+            className="border-2 border-black/20 bg-[#fff] p-2 text-black"
+            onChange={e => setUserIdSelected(parseInt(e.target.value))}
+          />
+        </label>
+        {userIdSelected && userData.data ? (
+          <>
+            <Typography.h2>Create booking for customer</Typography.h2>
+            <CreateBookingForm />
+            <Typography.h3>View/Amend booking for customer</Typography.h3>
+            <form action="">Form</form>
+          </>
+        ) : (
+          <>User not found</>
+        )}
       </section>
     </div>
   );
@@ -40,6 +66,13 @@ const CreateBookingForm = () => {
   const availableBookingsData = useGetAvailableBookingsQuery(filter);
   const availableBookings = availableBookingsData.data?.availableBookings;
 
+  const bookings = useAppSelector(selectBookings);
+  const dispatch = useAppDispatch();
+
+  const createBookings = () => {
+    if (!bookings.length) return toast.error('No bookings selected');
+  };
+
   if (!availableBookings) return null; // TODO: handle loading, error states
   return (
     <div className="bg-white p-3 text-black">
@@ -53,7 +86,7 @@ const CreateBookingForm = () => {
           availableBooking: booking,
           eventId: booking.event.id,
         }))}
-        title={<SelectedBookingsDropdown />}
+        title={<SelectedBookingsDropdown onBook={createBookings} />}
       />
     </div>
   );
